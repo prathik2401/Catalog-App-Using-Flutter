416843f9530a03383f3a14ecf1b52fb28bd68607
Sai Prathik R <saiprathik2419@gmail.com>
2023-09-09 06:01:57 +0530

    Day 6 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,15 +5,15 @@ import 'package:flutter_intro_project/utils/routes.dart';
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  final int day = 1;
-  final String name = "Prathik";
+  // final int day = 1;
+  // final String name = "Prathik";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("First Application"),
       ),
-      body: Center(child: Text("Welcome to Day $day of Flutter by $name.")),
+      body: const Center(child: Text("Welcome 👋")),
       drawer: Drawer(
         child: ListView(
           children: [
